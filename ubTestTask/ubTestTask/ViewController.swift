369486c2f64ae994baabec0f539fc9d802428ac8
Ivan <yavorin@ukr.net>
2025-04-26 15:53:29 +0300

    added a new project
    TODO: remove the Main storyboard and setup initial View Controller, CoreDats stack, project structure

---
+++ ubTestTask/ubTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ubTestTask
+//
+//  Created by Ivan_Tests on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
