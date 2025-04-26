8954cb92387e42ec465d10b6c6b750c3f24aca07
Ivan <yavorin@ukr.net>
2025-04-26 20:22:46 +0300

    Update ListScreenWorker.swift
    adding blueprints for further realisation

--- ubTestTask/ubTestTask/UI_Modules/ListScreen/ListScreenWorker.swift
+++ ubTestTask/ubTestTask/UI_Modules/ListScreen/ListScreenWorker.swift
@@ -5,14 +5,57 @@
 //  Created by Ivan_Tests on 26.04.2025.
 //
 
+typealias ListPostsCompletion = (Result<[PostListDataModel], any Error>) -> ()
 
 protocol ListScreenDataWorkerType {
-    
+    func fetchInitialData(completion:ListPostsCompletion)
+    func fetchNextPageData(completion:ListPostsCompletion)
+}
+
+protocol PostListDataModelStorage {
+    func getListPosts(page:Int, pageSize:Int) throws (PostListDataModelStorageError) -> [PostListDataModel]
+    func saveListPosts(_ posts:[PostListDataModel])
 }
 
 /**
  THis class is responsible for data loading and managing
  */
-class ListScreenWorker: ListScreenDataWorkerType {
+class ListScreenWorker<C:PostListDataModelStorage>: ListScreenDataWorkerType {
     
+    private(set) var pageSize:Int = 20
+    private var currentPage:Int = 0
+    private var cache:C
+    
+    init(pageSize: Int = 20, currentPage: Int = 0, cache: C) {
+        self.pageSize = pageSize
+        self.currentPage = currentPage
+        self.cache = cache
+    }
+    
+    func fetchInitialData(completion: ListPostsCompletion) {
+        precondition(currentPage == 0, "Not initial page data requested")
+        
+        fetch(completion: completion)
+    }
+    
+    func fetchNextPageData(completion: ListPostsCompletion) {
+        currentPage += 1
+        fetch(completion: completion)
+    }
+    
+    private func fetch(completion: ListPostsCompletion) {
+        do {
+            let fetchedBatch = try cache.getListPosts(page: self.currentPage,
+                                                      pageSize: self.pageSize)
+        }
+        catch {
+            switch error {
+            case .noData:
+                //try loading data
+                print("Trying to load some Posts for List...")
+            case .partialResult(let postListDataModels):
+                completion(.success(postListDataModels))
+            }
+        }
+    }
 }
